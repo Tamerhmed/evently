771bb97215b31239c2c131ab7c8e0dc474fa7e61
Tamer Ahmed <tamer30230@gmail.com>
2023-12-22 17:35:38 +1100

    add auth components

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <main>
+    <h1>hello app</h1>
+   </main>
+  )
+}
